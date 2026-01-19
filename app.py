3051e94b3cc577a6c6fcdb7f55a81102920aae08
lilin <707350162@qq.com>
2026-01-19 15:08:50 +0800

    canary版本

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello ci"
+    print("hahahahha")
+    return "hello canary"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
