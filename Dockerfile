5c890f5be22f4b15c210ad56b6842dc0839d5fad
lilin <707350162@qq.com>
2026-01-19 12:10:56 +0800

    改成页面 测试金丝雀发布

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3.11-slim
 WORKDIR /app
 COPY app.py .
+RUN pip install --no-cache-dir flask
+EXPOSE 8080
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello ci")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello ci"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
